21248fa9202b40817079cffe60d23daf2cd6eac4
Marc <purotx2014@gmail.com>
2020-01-14 21:51:06 -0500

    fixed pic

--- client/src/components/Slider/index.js
+++ client/src/components/Slider/index.js
@@ -9,35 +9,35 @@ function Slider() {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://doc-0c-7o-docs.googleusercontent.com/docs/securesc/je5tmjp1lmiag296orv7bs5v897l6df2/k19mcjvf6de8onhokrtqsrufsrshss22/1579039200000/09401388256139593411/09401388256139593411/1hAEDLeODyBxDHRLzU_2fg9o4qQ9fP6HP?authuser=0"
+      src="https://gdurl.com/W35g"
       alt="First slide"
     />
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://doc-10-7o-docs.googleusercontent.com/docs/securesc/je5tmjp1lmiag296orv7bs5v897l6df2/0dsaln60hh97adeuilcoberrj0o7mujc/1579032000000/09401388256139593411/09401388256139593411/1dbZ6mZXw6seSN_e5YBDeS1w3YFVqWAbR?authuser=0"
+      src="https://docs.google.com/uc?id=1dbZ6mZXw6seSN_e5YBDeS1w3YFVqWAbR"
       alt="Second slide"
     />
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://doc-10-7o-docs.googleusercontent.com/docs/securesc/je5tmjp1lmiag296orv7bs5v897l6df2/2dpqo00fd9dmtrgr00ue4e9untekqfk3/1579032000000/09401388256139593411/09401388256139593411/1ufOClc8uMLvnOT_a1c8z1e8flAxJOsGD?authuser=0"
+      src="https://docs.google.com/uc?id=1ufOClc8uMLvnOT_a1c8z1e8flAxJOsGD"
       alt="Third slide"
     />
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://doc-0g-7o-docs.googleusercontent.com/docs/securesc/je5tmjp1lmiag296orv7bs5v897l6df2/iuo6e1m6sqepbkviinmtjershs21t1r2/1579039200000/09401388256139593411/09401388256139593411/1habDjgitQQ9E6XUsOzFFdImT6n3N_7AC?authuser=0"
+      src="https://docs.google.com/uc?id=1habDjgitQQ9E6XUsOzFFdImT6n3N_7AC"
       alt="Fourth slide"
     />
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://doc-0o-7o-docs.googleusercontent.com/docs/securesc/je5tmjp1lmiag296orv7bs5v897l6df2/8567ab57tbhdq6uhnvqk50ojg40vvk39/1579032000000/09401388256139593411/09401388256139593411/1eKurdPR5qzfVs8sxGfP62Q8LiChfNx9I?authuser=0"
+      src="https://docs.google.com/uc?id=1hnOHhR_e_jJyG8xwnFJYApDFKefRP5q1"
       alt="Fifth slide"
     />
   </Carousel.Item>
