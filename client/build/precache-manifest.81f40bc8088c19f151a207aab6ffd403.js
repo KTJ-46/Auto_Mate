252012c71e4645e796eaf180c469cc7126d83469
Kshitij Vadake <ktj@Kshitijs-MacBook-Pro.local>
2020-01-23 21:34:51 -0500

    heroku deployment

---
+++ client/build/precache-manifest.81f40bc8088c19f151a207aab6ffd403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98b84756decc076fbf3",
+    "url": "/static/css/main.76845a5e.chunk.css"
+  },
+  {
+    "revision": "e98b84756decc076fbf3",
+    "url": "/static/js/main.b71e3690.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e5594c129698da807d4",
+    "url": "/static/js/2.15b21c93.chunk.js"
+  },
+  {
+    "revision": "72312730151e175837b0469ee51f7a96",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Loglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:Advance1@ds213079.mlab.com:13079/heroku_789bv3nh");
 
 // Start the API server
 app.listen(PORT, function() {
