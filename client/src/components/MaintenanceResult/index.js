6b8d08716cc39d97d441aa51a23dae205a43c175
Kshitij Vadake <ktj@Kshitijs-MacBook-Pro.local>
2020-01-23 20:10:36 -0500

    worked on maintenance page

---
+++ client/src/components/MaintenanceResult/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+//import "./style.css";
+import {Row, Col} from "../Grid"
+
+const MaintenanceResult = props => {
+    return (props.diagnostics.length === 0) ? (
+        <div className="card">
+            <div className="card-body player">
+                <div className="article">
+                    <h3>Maintenance Results</h3>
+                </div>
+            </div>
+        </div>
+    ) : (
+            <div className="card">
+                <div className="card-body player">
+                    <div className="article">
+                        <h3>Scheduled Maintenance Results</h3>
+                        {props.diagnostics.map(diagnostic => {
+                            return (
+                                <li className="search-list list-group-item" key={diagnostic.id}>
+                                    <Row className="SearchResult row" id={diagnostic.desc + "Card"}>
+                                       
+                                        <Col size="1" className="emptyCol"/>
+                                        {/* col-9 show information of the book */}
+                                        <Col size="9" className="Diagnosis">
+                                            
+                                            <Row>
+                                                <h3 className="diagDesc"> Diagnosis: {diagnostic.desc}</h3>
+                                            </Row>
+                                            <Row>
+                                                <h3 className="diagRepairHour"> Repair Hours needed for the job: {diagnostic.repairHours}</h3>
+                                            </Row>
+                                           
+                                            <Row>
+                                                <h4 className="diagLaborRate">Hourly Labor Rate: ${diagnostic.repairLaborRate}</h4>
+                                            </Row>
+                                            <Row>
+                                                <h3 className="diagPartsCost"> Cost of the parts: ${diagnostic.repairPartCost}</h3>
+                                            </Row>
+                                            <Row>
+                                                <p className="diagMiscCost">Miscellaneous/ Hazmat: ${diagnostic.repairMiscCost}</p>
+                                            </Row>
+                                            <Row>
+                                                <p className="diagTotalCost">Total Cost of the job: ${diagnostic.repairTotalCost}</p>
+                                            </Row>
+                                            <Row>
+                                                <p className="diagTSB">Technician Service Bulletin: {diagnostic.repairTSB}</p>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                    <br></br>
+                                    {/* <Row className="buttonDiv ">
+                                        <button className="saveBook btn btn-primary" id={book.id} onClick={(event) => props.handleSavedButton(event)}>
+                                            Save Book
+                                        </button>
+                                        <a href={book.link} target="_blank">
+                                            <button className="viewBook btn btn-success">
+                                                View Book
+                                        </button>
+                                        </a>
+                                    </Row> */}
+                                </li>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        )
+}
+export default MaintenanceResult
--- client/src/pages/Maintenance.js
+++ client/src/pages/Maintenance.js
@@ -1,39 +1,122 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
+import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
-import ContactUs from "../components/ContactUs";
+import Maintenance from "../components/Maintenance";
+import MaintenanceResult from "../components/MaintenanceResult";
 
-class Maintenance extends Component {
+class MaintenanceInfo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      vin: "",
+      mileage: "",
+      maintenance: [],
+      error: "",
+      message: ""
+    }
+  }
+        
 
- 
+        //function to take value of what enter in the text field for each input
+        handleInputChange = e => {
+          const {name, value} = e.target;
+          this.setState({ [name]: value })
+          }
+        handleFormSubmit = event => {
+          event.preventDefault();
+          console.log(this.state.vin)
+          // once it clicks it connects to the google book api with the search value
+          API.getCarMD(this.state.vin, this.state.mileage, this.state.dtc)
+              .then(res => {
+                  console.log('RAW RES=> ', res)
+                  // if (!Array.isArray(res)) {
+                  //   return 'results are not array'
+                  // }
+                  // else {
+                      // store response in a array
+                      let results = res.data.data
+                      //console log the results
+                      console.log('DATA from RESULT=> ', results);
+                      //map through the array 
+                      results = results.map((result, index) => {
+                          //store data/information in a new object 
+                          result = {
+                              key: index,
+                              id: index,
+                              desc: result.desc,
+                              repairHours: result.repair.hours,
+                              repairLaborRate: result.repair.labor_rate_per_hour,
+                              repairPartCost: result.repair.part_cost,
+                              repairLaborCost: result.repair.labor_cost,
+                              repairMiscCost: result.repair.misc_cost,
+                              repairTotalCost: result.repair.total_cost,
+                              repairTSB: result.tsb.file_url
+
+                            
+                          }
+                          return result;
+                      })
+                      // reset the sate of the empty books array to the new arrays of objects with properties geting back from the response
+                this.setState({ diagnosis: results, error: "" }, () => console.log('AFTER SETTING STATE',this.state.diagnosis))
+              })
+              .catch(err => this.setState({ error: err }));
+      }
+  
     render() {
+      console.log('REPAIR STATE on Render=>', this.state)
       if(!localStorage.getItem("token")) {
         return <Redirect to="/"/>
       } else {
-      return (
-        <Container fluid>
-          <Row>
-          <Col size="md-2">
-            <div className="sidebarMenu">
-            <br></br>
-            <br></br> 
-            <a href="/home" className="btn btn-secondary btn-block font-weight-bold" role="button">Home</a>
-            <br></br>
-            <br></br>
-            <a href="/repair" className="btn btn-secondary btn-block font-weight-bold" role="button">Repair/Diagnostic</a>
-            <br></br>
-            <br></br>
-            <a href="/logbook" className="btn btn-secondary btn-block font-weight-bold" role="button">Logbook</a>
-            <br></br>
-            <br></br>
-            <a href="/maintenance" className="btn btn-secondary btn-block font-weight-bold active" role="button">Maintenance</a> 
-             </div> 
+        return (
+          <Container fluid>
+            <Row>
+            <Col size="md-2">
+              <div className="sidebarMenu">
+              <br></br>
+              <br></br> 
+              <a href="/home" class="btn btn-secondary btn-block font-weight-bold" role="button">Home</a>
+              <br></br>
+              <br></br>  
+              <a href="/search" class="btn btn-secondary btn-block font-weight-bold" role="button">Search</a>
+              <br></br>
+              <br></br>
+              <a href="/repair" class="btn btn-secondary btn-block font-weight-bold active" role="button">Repair/Diagnostic</a>
+              <br></br>
+              <br></br>
+              <a href="/tsb" class="btn btn-secondary btn-block font-weight-bold" role="button">TSB</a>
+              <br></br>
+              <br></br>
+              <a href="/logbook" class="btn btn-secondary btn-block font-weight-bold" role="button">Logbook</a>
+              <br></br>
+              <br></br>
+              <a href="/maintenance" class="btn btn-secondary btn-block font-weight-bold" role="button">Maintenance</a> 
+              </div> 
+              </Col>
+              <Col size="md-10">
+
+                <Jumbotron>
+                  <h1 style={{marginTop: 50}} className="text-light">Get you Car's scheduled maintenance</h1>
+                </Jumbotron>                   
+              <Col size="md-6">
+                  <br></br>
+                  <br></br>
+                <Maintenance
+                    handleFormSubmit={this.handleFormSubmit}
+                    handleInputChange={this.handleInputChange}
+                />
+                <br></br>
+              </Col>
+                <br></br>               
+                <MaintenanceResult diagnostics={this.state.diagnosis} handleSavedButton={this.handleSavedButton} />          
             </Col>
+      
             </Row>
-    </Container>
-      );
+          </Container>
+        );
+      }
     }
   }
-} 
-  export default Maintenance;
+  
+  export default MaintenanceInfo;
