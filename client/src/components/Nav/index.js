0c2736e568eaa15d6e0ec00b95afc68de6856fef
Kshitij Vadake <ktj@Kshitijs-MacBook-Pro.local>
2020-01-25 12:42:00 -0500

    logout button is displayed on Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,32 @@
 import React from "react";
-
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 function Nav() {
   return (
+  function ActionLink() {
+    function handleClick(e) {
+      e.preventDefault();
+      console.log('The link was clicked.');
+    }
+  },
+ 
     <nav className="navbar bg-info">
       <h2>
         Auto-Mate
       </h2>
-    </nav>
-  );
+   
+ 
+ 
+      <ButtonToolbar>
+  <button  onClick={handleClick}  variant ="primary">Log Out</button>
+      </ButtonToolbar>
+    
+      
+      </nav>
+     );
+
+//   <button onClick={activateLasers}>
+//   Activate Lasers
+// </button>
 }
 
 export default Nav;
